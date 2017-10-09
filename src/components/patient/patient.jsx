6dfdf671eb8c5b6c4ef112bbb7fe54601fbb04fc
Michail Tsougkranis <mikenats@Michails-MBP.home>
2017-10-09 10:52:49 +0100

    patient path resolve

--- src/components/patient/patient.jsx
+++ src/components/patient/patient.jsx
@@ -2,8 +2,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import {sumOfObjArray, filterByAttribute, projectArrayToObject, extendObjectFromArray} from './patientlib';
-
+import {sumOfObjArray, filterByAttribute, projectArrayToObject, extendObjectFromArray} from './patientLib';
 
 /**
  * Component for  Patient
